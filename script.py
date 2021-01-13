ab6e792c85c32dd02949143760f63cf84e688942
Ilnur92 <73283380+Ilnur92@users.noreply.github.com>
2021-01-13 15:28:24 +0500

    Add files via upload

---
+++ script.py
@@ -0,0 +1,16 @@
+class Cat:
+    pass
+
+def __init__(self, name, floor, age):
+    self.name = name
+    self.floor = floor
+    self.age = age
+
+def getName (self):
+    return self.name
+
+def getFloor (self):
+    return self.floor
+
+def getAge (self):
+    return self.age
